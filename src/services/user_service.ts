a37931c34b576083ec690abd94ebfd37a503c9ae
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-05-03 16:37:55 +0200

    removed unnecessary async/await

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -5,16 +5,16 @@ import { CreateUserData } from "../types";
  * Creates a new user
  */
 
-export const createUser = async (data: CreateUserData) => {
-	return await prisma.user.create({ data })
+export const createUser = (data: CreateUserData) => {
+	return prisma.user.create({ data })
 }
 
 /**
  * When creating a new user, checks if email already is in use or
  * when login check if user exists in database
  */
-export const getUserByEmail = async (email: string) => {
-	return await prisma.user.findUnique({
+export const getUserByEmail = (email: string) => {
+	return prisma.user.findUnique({
 		where: { email }
 	})
 }
